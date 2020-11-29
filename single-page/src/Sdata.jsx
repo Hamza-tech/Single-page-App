90accefa74bf17d2ce8eb6fe98fc632ccae494a2
Hamza Khan <hamzaqasimkhan@gmail.com>
2020-11-29 15:18:31 +0500

    directory restructured

---
+++ single-page/src/Sdata.jsx
@@ -0,0 +1,39 @@
+import web from "../src/Images/web.jpg";
+import app from "../src/Images/software.jpg";
+import android from "../src/Images/android.jpg";
+import digital from "../src/Images/digital.jpg";
+import marketing from "../src/Images/marketing.jpg";
+import software from "../src/Images/software1.jpg";
+
+
+
+
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title : "Web Development",
+    },
+    {
+        imgsrc : app,
+        title : "App Development",
+    },
+    {
+        imgsrc : software,
+        title : "Software Development",
+    },
+    {
+        imgsrc : digital,
+        title : "Digital Marketing",
+    },
+    {
+        imgsrc : android,
+        title : "Android Development",
+    },
+    {
+        imgsrc : marketing,
+        title : "Marketing",
+    },
+];
+
+export default Sdata;
